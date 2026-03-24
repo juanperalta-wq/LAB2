881a287a68d800094163b055138492f7ccd726df
juanperalta-wq <juan.peralta@tecsup.edu.pe>
2026-03-24 12:12:16 -0500

    enemy avance

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform Player;
+    private NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.speed = Random.Range(1,10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            agent.SetDestination(Player.position);
+            //agent.
+
+        }
+    }
+    public void HasPath()
+    {
+        print(agent.hasPath);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (agent == null || agent.path == null) return;
+
+        Vector3[] corners = agent.path.corners;
+
+
+        for (int i = 0; i < corners.Length - 1; i++)
+        {
+            Gizmos.DrawLine(corners[i], corners[i + 1]);
+            Gizmos.DrawSphere(corners[i], 0.2f);
+        }
+    }
+}
